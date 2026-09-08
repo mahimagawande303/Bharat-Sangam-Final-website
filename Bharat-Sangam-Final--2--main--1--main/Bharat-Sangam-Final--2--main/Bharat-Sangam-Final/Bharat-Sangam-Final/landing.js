00614acb1da50a3e1ae7025ae9176742b9cad631
mahimagawande303 <mahimagawande303@gmail.com>
2026-09-08 23:59:12 +0530

    first commit

---
+++ Bharat-Sangam-Final--2--main--1--main/Bharat-Sangam-Final--2--main/Bharat-Sangam-Final/Bharat-Sangam-Final/landing.js
@@ -0,0 +1,1 @@
+document.querySelectorAll(".nav-links a").forEach(a=>a.addEventListener("click",()=>{document.querySelectorAll(".nav-links a").forEach(x=>x.classList.remove("active"));a.classList.add("active")}));
